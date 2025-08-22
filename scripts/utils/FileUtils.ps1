a5d0d09ad9beda2526715d4d97bc5d4edbe19de4
Jad Ismail <ismailjad@microsoft.com>
2025-08-22 09:15:42 +0200

    Add license header to scripts

--- scripts/utils/FileUtils.ps1
+++ scripts/utils/FileUtils.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
 function Remove-Directory {
     param (
         [string]$directoryPath
--- scripts/utils/PowerAppsUtils.ps1
+++ scripts/utils/PowerAppsUtils.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
 function Load-SolutionXml {
     param (
         [string]$xmlPath
